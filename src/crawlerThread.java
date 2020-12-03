5b1647a0fc59c71f1523d1a19bcc1bf4020eb222
vasi-oniga-selab <vasile.oniga@mta.ro>
2020-12-03 13:07:04 +0200

    Implementeaza schelet multithreading
    Issue: #4
    Implementează scheletul ce va fi folosit pentru a realiza
    programarea în paralel a programului.
    
    Signed-off-by: vasi-oniga-selab <vasile.oniga@mta.ro>

---
+++ src/crawlerThread.java
@@ -0,0 +1,10 @@
+import java.net.URL;
+import java.util.ArrayList;
+
+public   class crawlerThread {
+    public void start(){;}
+    public URLQueue getQ(){}
+    public ArrayList<Task> getTasks(){}
+    public void addTask(Task _toAdd){;}
+    public void removeTask(Task _toDelete){;}
+}
