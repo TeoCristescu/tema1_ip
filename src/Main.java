46b70fb2a52549b604c1189505769f1303d6d0d1
lacramioara <runcan_lacramioara@yahoo.com>
2020-12-08 20:45:37 +0200

    Am implementat clasa FilterBySizeTask.
    Issue: #3
    Afiseaza toate fisierele care au size-ul mai mare decat cel specificat.
    
    Signed-off-by: lacramioara <runcan_lacramioara@yahoo.com>

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,12 @@
+package WebCrawler.tema1_ip.src;
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args)
-    {
-        Crawler.run(args);
+    public static void main(String[] args) throws DirectoryNotFoundException, IOException {
+        //Crawler.run(args);
+        Task task1=new FilterBySizeTask("exemple/",173402);
+        task1.execute();
+
     }
 
 }
--- src/SitemapTask.java
+++ src/SitemapTask.java
@@ -1,6 +1,15 @@
+package WebCrawler.tema1_ip.src;
+
+import java.io.IOException;
+
 public class SitemapTask implements Task {
     public static void generateSitemap(String word)
     {
+
+    }
+
+    @Override
+    public void execute() throws DirectoryNotFoundException, IOException {
 
     }
 }
