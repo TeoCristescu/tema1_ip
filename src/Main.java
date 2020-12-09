78a32764d684cce6155a960b062737d086ff948b
vasi-oniga-selab <vasioniga@gmail.com>
2020-12-09 22:17:43 +0200

    Download Documente
    issue-26-download-documente
    Am implementat descărcarea de documente
    Signed-off-by: vasi-oniga-selab <vasioniga@gmail.com>

--- src/Main.java
+++ src/Main.java
@@ -9,8 +9,8 @@ public class Main {
     public static void main(String[] args)  {
 
         Crawler.run(args);
-        String myUrl="https://www.emag.ro/set-bakugan-battle-pack-ventus-phaedrus-si-pyrus-hydranoid-cu-5-figurine-incluse-6427037016615/pd/DVD4K7MBM/?ref=graph_profiled_similar_d_1_2&provider=rec&recid=rec_49_16_u2029049839059478178_807_D_3512debeebc80a515c5f233e1061b95d3d7b77546541c53a779832d517ac6563_1607520357&scenario_ID=49#product-gallery";
-        DownloadTask dw=new DownloadTask(4,myUrl,".pdf#.png",1);
+        String myUrl="http://www.dhs.state.il.us/OneNetLibrary/27897/documents/Initiatives/IITAA/Sample-Document.docx";
+        DownloadTask dw=new DownloadTask(4,myUrl,".pdf#.png#.docx#.jpg",1);
         dw.execute();
         dw.listURLQ();
 
