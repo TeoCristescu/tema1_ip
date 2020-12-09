99f949e23bb5d19d64751201654a37d40defaf1c
TeoCristescu <teocrriss99@gmail.com>
2020-12-09 15:14:16 +0200

    Implementare clasa ListTask
    Issue: #2
    Am implementat clasa ListTask
    
    Signed-off-by: TeoCristescu <teocrriss99@gmail.com>

--- src/ListTask.java
+++ src/ListTask.java
@@ -1,6 +1,46 @@
+import java.util.ArrayList;
+import java.util.Random;
+import java.io.File;
+import java.io.FilenameFilter;
 public class ListTask implements Task{
+    private String mTip;
+    private String mCale;
+    ArrayList<String> lista_fisiere = new ArrayList<String>();
+    public ListTask(String type, String path)
+    {
+        this.mTip = type;
+        this.mCale = path;
+    }
     public void execute()
     {
+        File directoryPath = new File(mCale);
+        try {
 
+            searchInDirector(directoryPath, mTip);
+            if(lista_fisiere.size()==0)
+            {
+                System.out.print("\n Nu s-au gasit fisiere de acest tip \n");
+            }
+            else
+            {
+                System.out.print("\n Fisierele de acest tip sunt:  \n");
+                System.out.print(lista_fisiere);
+                System.out.print("\n");
+            }
+
+        } catch (Exception e) {
+            System.out.print("\n Directorul nu exista \n");
+        }
     };
+    private void searchInDirector(File f,String tip) {
+        File[] a = f.listFiles();
+
+        for (File x : a) {
+            if (x.isDirectory()) {
+                searchInDirector(x, tip);
+            } else if (x.getName().endsWith(tip)) {
+                lista_fisiere.add(x.getName());
+            }
+        }
+    }
 }
