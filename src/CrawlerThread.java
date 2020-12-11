baee5f40483c4b30dca8e3c0c1470d2fc9687d7b
vasi-oniga-selab <vasioniga@gmail.com>
2020-12-11 19:12:38 +0200

    De testat+patch.Ver. 0.0.1
    issue-26-download-documente
    Patch download-task+teoretic prima versiune stabilă.
    Signed-off-by: vasi-oniga-selab <vasioniga@gmail.com>

--- src/CrawlerThread.java
+++ src/CrawlerThread.java
@@ -21,6 +21,7 @@ public   class CrawlerThread extends Thread {
         //BufferedReader reader;
         CrawlerManager crawler_instance = CrawlerManager.getInstance();
         String root_dir = crawler_instance.getRoot_dir();
+
         for(int i=start;i<end;i++)
         {
             URL url = downloadPending.getLinksList(i);
@@ -31,15 +32,13 @@ public   class CrawlerThread extends Thread {
             //String names[]=calea[1].split("/",0);//
             //int nrNames=names.length;
             String calea2=calea[1].replaceAll("/","\\\\");
-            int lenCalea2=calea2.length();
-
+            calea2=root_dir+"\\"+calea2;
+            System.out.println("Calea: "+calea2);
             try {
 
                 //reader = new BufferedReader(new InputStreamReader(url.openStream()));
                // BufferedWriter writer = null;
-                InputStream in = url.openStream();
-                int length = -1;
-                byte[] buffer = new byte[4096];
+
                 String theExtension=".html";
                 String[]extensionsString=this.extensions.split("#",0);
 
@@ -47,7 +46,7 @@ public   class CrawlerThread extends Thread {
                     {
                         int sizeExtensie = extensionsString[k].length();
                         String aux_url = calea2.substring(calea2.length() - sizeExtensie);
-                        System.out.println("extensionsString[k] "+extensionsString[k]+" aux_url "+aux_url);
+                        //System.out.println("extensionsString[k] "+extensionsString[k]+" aux_url "+aux_url);
                         if ((extensionsString[k].equals(aux_url)))
                         {
                             theExtension = extensionsString[k];
@@ -58,42 +57,85 @@ public   class CrawlerThread extends Thread {
 
                 System.out.println("extension "+theExtension);
                 System.out.println("calea "+calea2);
-                //writer = new BufferedWriter(new FileWriter(calea2+"index"+i+theExtension));
-                File f = new File(calea2+"index"+i+theExtension);
-                f.getParentFile().mkdirs();
-                try {
-                    f.createNewFile();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-
-                FileOutputStream fos = new FileOutputStream(f);
-
-                // System.out.println("pagina "+i+" este "+url.toString());
-                while ((length = in.read(buffer)) > -1) //(line=reader.readLine())!=null
+                if(theExtension.equals(".html"))
                 {
-                    String myLink = new String(buffer);
-                    boolean needs_replacement = false;
-                    if(myLink.contains("href=\"http://")) {
-                        myLink = myLink.replace("href=\"http://", "href=\"" + root_dir + "\\");
-                        needs_replacement = true;
+
+                    File f = new File(calea2 + "index" + i + theExtension);
+                    f.getParentFile().mkdirs();
+                    try {
+                        f.createNewFile();
+                    } catch (IOException e) {
+                        e.printStackTrace();
                     }
-                    if(myLink.contains("href=\"https://")) {
-                        myLink = myLink.replace("href=\"https://", "href=\"" + root_dir + "\\");
-                        needs_replacement = true;
+                    BufferedReader reader =  new BufferedReader(new InputStreamReader(url.openStream()));
+                    BufferedWriter writer = new BufferedWriter(new FileWriter(calea2 + "index" + i + theExtension));
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+
+                        String myLink = line;
+                        boolean needs_replacement = false;
+                        if (myLink.contains("href=\"http://")) {
+                            myLink = myLink.replace("href=\"http://", "href=\"" + root_dir + "\\");
+                            needs_replacement = true;
+                        }
+                        if (myLink.contains("href=\"https://")) {
+                            myLink = myLink.replace("href=\"https://", "href=\"" + root_dir + "\\");
+                            needs_replacement = true;
+                        }
+
+                        if (needs_replacement) {
+
+                            writer.write(myLink);
+                        }
+                        else {
+
+                            writer.write(line);
+                        }
                     }
 
-                    if(needs_replacement)
-                        fos.write(myLink.getBytes(), 0, myLink.length());
-                    else
-                        fos.write(buffer, 0, length);
                 }
-                fos.close();
-                in.close();
+                else {
+
+                    InputStream in = url.openStream();
+                    int length = -1;
+                    byte[] buffer = new byte[4096];
+                    //writer = new BufferedWriter(new FileWriter(calea2+"index"+i+theExtension));
+                    File f = new File(calea2 + "index" + i + theExtension);
+                    f.getParentFile().mkdirs();
+                    try {
+                        f.createNewFile();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                    FileOutputStream fos = new FileOutputStream(f);
+
+                    // System.out.println("pagina "+i+" este "+url.toString());
+                    while ((length = in.read(buffer)) > -1) //(line=reader.readLine())!=null
+                    {
+                        String myLink = new String(buffer);
+                        boolean needs_replacement = false;
+                        if (myLink.contains("href=\"http://")) {
+                            myLink = myLink.replace("href=\"http://", "href=\"" + root_dir + "\\");
+                            needs_replacement = true;
+                        }
+                        if (myLink.contains("href=\"https://")) {
+                            myLink = myLink.replace("href=\"https://", "href=\"" + root_dir + "\\");
+                            needs_replacement = true;
+                        }
+
+                        if (needs_replacement)
+                            fos.write(myLink.getBytes(), 0, myLink.length());
+                        else
+                            fos.write(buffer, 0, length);
+                    }
+                    fos.close();
+                    in.close();
+                }
             }
             catch ( Exception e1)
             {
-                System.out.println("Pagina nu există:");
+                System.out.println("Pagina nu exista:");
                 System.out.println(e1.toString());
             }
         }
