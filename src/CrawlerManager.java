ab26b040e7f8d440266e583c12a9fc8776740201
gabriel-avram-selab <gabrielavram1234@gmail.com>
2020-12-14 01:27:46 +0200

    issue: #31
    Am adaugat comentarii conform java coding style si mesaje de log.
    
    Signed-off-by: gabriel-avram-selab <gabrielavram1234@gmail.com>

--- src/CrawlerManager.java
+++ src/CrawlerManager.java
@@ -2,6 +2,12 @@ import java.io.FileNotFoundException;
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
+
+/**
+ * Clasa CrawlerManager este unica la nivelul programului si se ocupa cu salvarea argumentelor introduse
+ * de utilizator.
+ * @author Avram Gabriel
+ */
 public class CrawlerManager {
 
     private int threads_nr;
@@ -20,6 +26,11 @@ public class CrawlerManager {
 
     private CrawlerManager() { }
 
+    /**
+     *
+     * Aceasta funcție accesează membrul LogManager care salvează in fisierul de log
+     * toate activitățile aplicatiei
+     */
     static void write2logfile(String message)  {
         try{
             log.writeToLogFile(message);
@@ -36,13 +47,30 @@ public class CrawlerManager {
         return obj;
     }
 
+    /**
+     *
+     *  Aceasta functie initializeaza Crawlermanager cu calea către directorul in care se cere cautarea si
+     *  un cuvant anume.
+     */
     public void search_config(String searcharg,String root){
         search_argument=searcharg;
         root_dir=root;
     }
+
+    /**
+     * Aceasta functie initializeaza Crawlermanager cu tipul de fisier pentru care se doreste
+     * listarea si directorul de start.
+     */
     public void list_config(String item,String root){
         list_argument=item;root_dir=root;
     }
+
+    /**
+     * Această funcție citeste fisierul de configurare si extrage inormațiile necesare rulării:
+     * numărul de threaduri, extensiile dorite, URL-ul de start si adancimea parcurgerii.
+     * @param configfile este fisierul de configurare.
+     *
+     */
     public void crawl_config(String configfile) throws FileNotFoundException, ExceptieArgumente {
 
         File f=new File(configfile);
@@ -63,16 +91,27 @@ public class CrawlerManager {
             } else if (line.contains("extensions")) {
                 extensions = line.substring(11);
             } else {
+                CrawlerManager.write2logfile("ERROR: Option not recognised in config file!");
                 throw new ExceptieArgumente("Option not recognised in config file");
             }
         }
     }
+
+    /**
+     *
+     * @param type este tipul de task care va fi creat.
+     */
     public void setType(String type) {
         this.type = type;
     }
     public void setRoot_dir(String root_dir) {
         this.root_dir = root_dir;
     }
+
+    /**
+     *
+     * @param filter_size este dimensiunea in octeti dupa care se doreste filtrarea documentelor.
+     */
     public void setFilter_size(int filter_size) {
         this.filter_size = filter_size;
     }
